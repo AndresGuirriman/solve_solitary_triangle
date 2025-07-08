7c7ded9b3fad0ad0c565907ca2be1fbf67dec225
AndresGuirriman <aguirriman@gmail.com>
2025-07-08 17:14:27 -0400

    first commit, in process for solver

---
+++ config.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+
+# ruta de la carpeta
+pathM = '/Users/andresguirrimanalvarez/Documents/proyectos_autodidactas/juego_piramide'
+
+# Definimos variables que luego usaremos como testers
+A = np.array([
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 1, 1]
+])
+
+B = np.array([
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0]
+])
+
+C = np.array([
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1]
+])
+
+Z = np.array([
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0]
+])
+
+bad = np.array([
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0]
+])
+
+not_square = np.array([
+    [1, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+    ])
---
+++ functions.py
@@ -0,0 +1,32 @@
+import numpy as np  
+import pandas as pd  
+
+def is_lower_triangle(matrix):
+    # Esta funcion retorna False si no es una matriz cuadrada; True si lo es
+    (filas, columnas) = matrix.shape
+    # Verifiquemos que es una matriz cuadrada
+    if filas != columnas:
+        return False
+    
+    # variable que facilita ver si la matriz es vacia:
+    sum = 0
+
+    for i in range(filas):
+        for j in range(filas):
+            sum += matrix[i,j]
+            if matrix[i,j] != 0 and matrix[i,j] != 1: # Vemos que solo haya 1s o 0s
+                return False
+            if i < j and matrix[i,j] == 1: # si nos salimos del triangulo, tiene que ser un 0.
+                return False
+    
+    # aqui confirmamos que no sea vacia
+    if sum == 0:
+        return False
+
+    return True
+
+def n_per_file(matrix):
+
+    n = 0
+
+    return
